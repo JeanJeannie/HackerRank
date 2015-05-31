4ec2d8ecba91c68cea6c0715eedb364f4f474eb2
Jean <jmcclymont@gmail.com>
2015-05-31 21:02:59 +1200

    Accepted answer for FillingJars

--- HackerRank/Algorithm/WarmUpEasy.cs
+++ HackerRank/Algorithm/WarmUpEasy.cs
@@ -8,6 +8,127 @@ namespace HackerRank.AlgorithmWarmUpEasy
 {
    public class WarmUpEasy
    {
+
+      public static void SherlockAndTheBeast(int numOfTests)
+      {
+         for (int testNo = 0; testNo < numOfTests; testNo++)
+         {
+            var numOfDigits = Convert.ToInt32(Console.ReadLine());
+            var result = GetFivesAndThrees(numOfDigits);
+            Console.WriteLine(result.OutputString());
+         }
+      }
+
+      /**
+       * 3 : 555
+       * 5 : 33,333
+       * 6 : 555,555
+       * 7 : -1
+       * 8 : 55,533,333  => 8%3=2 => (2+3)%5= 0 1%5 = 0
+       * 9 : 555,555,555  => 9%3=0
+       * 10 : 3,333,333,333  => 10%3 = 1 => 
+       * 
+       * 
+       * **/
+
+      public static FivesAndThrees GetFivesAndThrees(int numOfDigits)
+      {
+         var result = new FivesAndThrees(numOfDigits);
+         result.Process();
+         return result;
+      }
+
+      public class FivesAndThrees
+      {
+         public int NumOfFives { get; set; }
+         public int NumOfThrees { get; set; }
+         public int NumOfDigits { get; set; }
+
+
+         public FivesAndThrees(int numOfDigits)
+         {
+            NumOfDigits = numOfDigits;
+            NumOfFives = 0;
+            NumOfThrees = 0;
+         }
+
+         public void Process()
+         {
+            if (NumOfDigits == 3 || NumOfDigits % 3 == 0)
+            {
+               NumOfFives = NumOfDigits;
+               return;
+            }
+
+            if (NumOfDigits == 5)
+            {
+               NumOfThrees = NumOfDigits;
+               return;
+            }
+
+            if (NumOfDigits < 5)
+            {
+               return;
+            }
+
+            var resultFound = false;
+            NumOfFives = NumOfDigits;
+            NumOfThrees = 0;
+            while (!resultFound)
+            {
+               if (NumOfFives % 3 == 0 && NumOfThrees % 5 == 0)
+               {
+                  return;
+               }
+
+               // take 1 off of the tmpNumFives and retry
+               NumOfFives--;
+               NumOfThrees++;
+
+               if (NumOfFives == 0) //|| NumOfThrees == 0)
+               {
+                  if (NumOfThrees > 0 && NumOfDigits % 5 == 0)
+                     NumOfThrees = NumOfDigits;
+                  else
+                     NumOfThrees = 0;
+                  return;
+               }
+            }
+         }
+         /**
+      * 3 : 555
+      * 5 : 33,333
+      * 6 : 555,555
+      * 7 : -1
+      * 8 : 55,533,333  => 8%3=2 => (2+3)%5= 0 1%5 = 0
+      * 9 : 555,555,555  => 9%3=0
+      * 10 : 3,333,333,333  => 10%3 = 1 => 
+      * 
+      * 
+      * **/
+
+
+         public StringBuilder OutputString()
+         {
+            var builder = new StringBuilder();
+            for (int i = 0; i < NumOfFives; i++)
+            {
+               builder.Append("5");
+            }
+
+            for (int i = 0; i < NumOfThrees; i++)
+            {
+               builder.Append("3");
+            }
+
+            if (NumOfThrees == 0 && NumOfFives == 0)
+            {
+               builder.Append("-1");
+            }
+            return builder;
+         }
+      }
+
       public class gcdDigit
       {
          public long Digit { get; set; }
--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -13,128 +13,25 @@ namespace HackerRank
    class Solution
    {
       static void Main(string[] args)
-      {
-        
-         SherlockAndTheBeast(Convert.ToInt32(Console.ReadLine().ToString()));
+      {        
+         FillingJars();
       }
 
-      public static void SherlockAndTheBeast(int numOfTests)
+      public static void FillingJars()
       {
-         for (int testNo = 0; testNo < numOfTests; testNo++)
+         var tmpInput = Console.ReadLine().Split(' ').Select(s => Convert.ToInt64(s)).ToArray();
+         var numOfJars = tmpInput[0];
+         var numOfOperations = tmpInput[1];
+
+         long totalSum = 0;
+         for (int opNum = 0; opNum < numOfOperations; opNum++)
          {
-            var numOfDigits = Convert.ToInt32(Console.ReadLine());
-            var result = GetFivesAndThrees(numOfDigits);
-            Console.WriteLine(result.OutputString());
+            tmpInput = Console.ReadLine().Split(' ').Select(s => Convert.ToInt64(s)).ToArray();
+            var totalThisOp = (tmpInput[1] - tmpInput[0] + 1) * tmpInput[2];
+            totalSum = totalSum + totalThisOp;
          }
-      }
-
-      /**
-       * 3 : 555
-       * 5 : 33,333
-       * 6 : 555,555
-       * 7 : -1
-       * 8 : 55,533,333  => 8%3=2 => (2+3)%5= 0 1%5 = 0
-       * 9 : 555,555,555  => 9%3=0
-       * 10 : 3,333,333,333  => 10%3 = 1 => 
-       * 
-       * 
-       * **/
-
-      public static FivesAndThrees GetFivesAndThrees(int numOfDigits)
-      {
-         var result = new FivesAndThrees(numOfDigits);
-         result.Process();
-         return result;
-      }
-
-      public class FivesAndThrees {
-         public int NumOfFives { get; set; }
-         public int NumOfThrees { get; set; }
-         public int NumOfDigits { get; set; }
-
-
-         public FivesAndThrees(int numOfDigits)
-         {
-            NumOfDigits = numOfDigits;
-            NumOfFives = 0;
-            NumOfThrees = 0;
-         }
-
-         public void Process()
-         {
-            if (NumOfDigits == 3 || NumOfDigits%3 == 0)
-            {
-               NumOfFives = NumOfDigits;
-               return;
-            }
-
-            if (NumOfDigits == 5)
-            {
-               NumOfThrees = NumOfDigits;
-               return;
-            }
-
-            if (NumOfDigits < 5)
-            {
-               return;
-            }
-
-            var resultFound = false;
-            NumOfFives = NumOfDigits;
-            NumOfThrees = 0;
-            while (!resultFound)
-            {
-               if (NumOfFives % 3 == 0 && NumOfThrees % 5 == 0)
-               {
-                  return;
-               }
-
-               // take 1 off of the tmpNumFives and retry
-               NumOfFives--;
-               NumOfThrees++;
-
-               if (NumOfFives == 0) //|| NumOfThrees == 0)
-               {
-                  if (NumOfThrees > 0 && NumOfDigits % 5 == 0)
-                     NumOfThrees = NumOfDigits;
-                  else
-                     NumOfThrees = 0;
-                  return;
-               }
-            }
-         }
-         /**
-      * 3 : 555
-      * 5 : 33,333
-      * 6 : 555,555
-      * 7 : -1
-      * 8 : 55,533,333  => 8%3=2 => (2+3)%5= 0 1%5 = 0
-      * 9 : 555,555,555  => 9%3=0
-      * 10 : 3,333,333,333  => 10%3 = 1 => 
-      * 
-      * 
-      * **/
-
-         
-         public StringBuilder OutputString()
-         {
-            var builder = new StringBuilder();
-            for (int i = 0; i < NumOfFives; i++)
-            {
-               builder.Append("5");
-            }
-
-            for (int i = 0; i < NumOfThrees; i++)
-            {
-               builder.Append("3");
-            }
-
-            if (NumOfThrees == 0 && NumOfFives == 0)
-            {
-               builder.Append("-1");
-            }
-            return builder;
-         }
+         var average = Math.Floor((decimal)totalSum / (decimal)numOfJars);
+         Console.WriteLine(average);
       }
    }
 }
