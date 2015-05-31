ff9710673d0c6be5e70af2c1573dd12e8572ea80
Jean <jmcclymont@gmail.com>
2015-06-01 11:06:24 +1200

    Accepted answer for IsFibo

--- HackerRank/Algorithm/WarmUpEasy.cs
+++ HackerRank/Algorithm/WarmUpEasy.cs
@@ -8,6 +8,38 @@ namespace HackerRank.AlgorithmWarmUpEasy
 {
    public class WarmUpEasy
    {
+      public static void ModifiedKaprekarNumbers()
+      {
+         var lowestNumber = Convert.ToInt32(Console.ReadLine());
+         var highestNumber = Convert.ToInt32(Console.ReadLine());
+
+         var listKaprekar = new List<int>();
+         for (int i = lowestNumber; i <= highestNumber; i++)
+         {
+            if (KaprekarNumber(i))
+               listKaprekar.Add(i);
+         }
+         if (listKaprekar.Any())
+            Console.WriteLine(string.Join(" ", listKaprekar.ToArray()));
+         else
+            Console.WriteLine("INVALID RANGE");
+      }
+
+      public static bool KaprekarNumber(int num)
+      {
+         var sqrNum = Math.Pow(num, 2);
+         var textSqrNum = sqrNum.ToString(); //.ToCharArray().Select(s => Convert.ToInt32(s.ToString())).ToArray();
+
+         var leftBit = textSqrNum.Substring(0, (int)Math.Floor((double)textSqrNum.Length / 2));
+         var rightBit = textSqrNum.Substring((int)Math.Floor((double)textSqrNum.Length / 2));
+
+         //     Console.WriteLine("sqr [{0}] left [{1}] right [{2}]", textSqrNum, leftBit, rightBit);
+         if (Convert.ToInt32(rightBit) + (leftBit == "" ? 0 : Convert.ToInt32(leftBit)) == num)
+            return true;
+         return false;
+      }
+
+
       public static void TimeInWords()
       {
          var hour = Convert.ToInt32(Console.ReadLine());
--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -14,39 +14,41 @@ namespace HackerRank
    {
       static void Main(string[] args)
       {
-         ModifiedKaprekarNumbers();            
+         IsFibo(Convert.ToInt32(Console.ReadLine()));            
+//         Console.ReadLine();
       }
 
-      public static void ModifiedKaprekarNumbers()
+      public static void IsFibo(int numTestCases)
       {
-         var lowestNumber = Convert.ToInt32(Console.ReadLine());
-         var highestNumber = Convert.ToInt32(Console.ReadLine());
-
-         var listKaprekar = new List<int>();
-         for (int i = lowestNumber; i <= highestNumber; i++)
+         for (int i = 0; i < numTestCases; i++)
          {
-            if (KaprekarNumber(i))
-               listKaprekar.Add(i);
+            var num = Convert.ToInt64(Console.ReadLine());
+            if (IsFibonacciNumber(num))
+               Console.WriteLine("IsFibo");
+            else
+               Console.WriteLine("IsNotFibo");
          }
-         if (listKaprekar.Any())
-            Console.WriteLine(string.Join(" ", listKaprekar.ToArray()));
-         else
-            Console.WriteLine("INVALID RANGE");
       }
 
-      public static bool KaprekarNumber(int num)
+      public static bool IsFibonacciNumber(long num)
       {
-         var sqrNum = Math.Pow(num, 2);
-         var textSqrNum = sqrNum.ToString(); //.ToCharArray().Select(s => Convert.ToInt32(s.ToString())).ToArray();
+         long currNum = 1;
+         long lastNum = 0;
+         long fiboNum = currNum + lastNum;
 
-         var leftBit = textSqrNum.Substring(0, (int)Math.Floor((double)textSqrNum.Length / 2));
-         var rightBit = textSqrNum.Substring((int)Math.Floor((double)textSqrNum.Length / 2));
-
-    //     Console.WriteLine("sqr [{0}] left [{1}] right [{2}]", textSqrNum, leftBit, rightBit);
-         if (Convert.ToInt32(rightBit) + (leftBit == ""? 0 : Convert.ToInt32(leftBit)) == num)
+         if (num == currNum || num == lastNum)
             return true;
-         return false;
-      }
 
+         while (fiboNum <= num)
+         {
+            fiboNum = currNum + lastNum;
+            lastNum = currNum;
+            currNum = fiboNum;
+
+            if (num == fiboNum)
+               return true;
+         }
+         return false;        
+      }
    }
 }
