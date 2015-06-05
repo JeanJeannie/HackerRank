ed2c631411693d4fbfb98ac1d8c183f1788b5963
Jean <jmcclymont@gmail.com>
2015-06-05 22:42:59 +1200

    Accepted answer for IsPalindromic

--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -14,44 +14,65 @@ namespace HackerRank
    {
       static void Main(string[] args)
       {
-         BiggerIsGreater(Convert.ToInt32(Console.ReadLine()));
-       //          Console.ReadLine();
+         PalindromeIndex(Convert.ToInt32(Console.ReadLine()));
+        // Console.ReadLine();
       }
 
-      public static void BiggerIsGreater(int numOfTestCases)
+      public static void PalindromeIndex(int numOfTestCases)
       {
          for (int testNo = 0; testNo < numOfTestCases; testNo++)
          {
-            var inputString = Console.ReadLine(); //.ToCharArray();
-            var outputString = "no answer";
-            var stop = false;
-            for (int i = inputString.ToCharArray().Length-2; i >= 0 ; i--)
-            {
-               var subset = inputString.Substring(i).ToCharArray();
-               var currChar = inputString[i]; // start with last 2 currs
-               for (int j = 1; j < subset.Length; j++)  // cur char should be at pos 0 so skip it
-               {
-                  if (currChar < subset[j])
-                  {
-                     // we need to find the next greatest char
-                     var nextGreatestChar = subset.Where(w => w > currChar).OrderBy(o => o).First();
-                     var pos = (new string(subset)).IndexOf(nextGreatestChar);
-                     var decreasedSubset = (new string(subset)).Remove(pos, 1).ToCharArray().OrderBy(o => o).ToArray();
-                     var left = inputString.Substring(0, i);
-                     var right = new string(decreasedSubset);
-                     outputString = left + nextGreatestChar.ToString() + right;
-                     stop = true;
-                     break;
-                  }
-               }
+            var inputString = Console.ReadLine();
+            Console.WriteLine(IsPalindromic(inputString));
 
-               if (stop)
-                  break;                              
+         }
+      }
+
+      public static bool FastLook(string inputString, int leftStart, int rightStart, out int? leftEnd, out int? rightEnd)
+      {
+         leftEnd = null;
+         rightEnd = null;
+         var left = leftStart;
+         var right = rightStart;
+         var tmpString = inputString.ToCharArray();
+
+         while (left <= right)
+         {
+            if (tmpString[left] != tmpString[right])
+            {
+               leftEnd = left;
+               rightEnd = right;
+               return false;
+            }
+            left++;
+            right--;
+         }
+         return true;
+      }
+
+      public static int IsPalindromic(string inputString)
+      {
+         var leftPos = 0;
+         var rightPos = inputString.Length-1;
+         int? outLeft;
+         int? outRight;
+
+         if (!FastLook(inputString, leftPos, rightPos, out outLeft, out outRight))
+         {
+            leftPos = (int)outLeft;
+            rightPos = (int)outRight;
+            if (FastLook(inputString, leftPos + 1, rightPos, out outLeft, out outRight))
+            {
+               return (int)leftPos;
             }
 
-            Console.WriteLine(outputString);
-            
+            if (FastLook(inputString, leftPos, rightPos - 1, out outLeft, out outRight))
+            {
+               return (int)rightPos;
+            }
+            return 0;
          }
+         return -1;
       }
    }
 }
