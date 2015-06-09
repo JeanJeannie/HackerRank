5cae6bf4873bbae4d72241414f624fa09a489dae
Jean McClymont <jean.mcclymont@xero.com>
2015-06-09 12:42:24 +1200

    Accepted answer for StrangeGrid

--- HackerRank/Algorithm/Algorithm_Combinatorics.cs
+++ HackerRank/Algorithm/Algorithm_Combinatorics.cs
@@ -17,5 +17,25 @@ namespace HackerRank.Algorithm
          }
       }
 
+      public static void Handshakes(int numOfTestCases)
+      {
+          for (int testNo = 0; testNo < numOfTestCases; testNo++)
+          {
+              var people = Convert.ToInt64(Console.ReadLine());
+              long handshakes = 0;
+
+              if (people >= 2)
+              {
+                  var half = Convert.ToInt64(Math.Ceiling((double)people / 2));
+                  handshakes = ((half - 1) * people);
+                  if (people % 2 == 0)
+                  {
+                      handshakes = handshakes + half;
+                  }
+              }
+              Console.WriteLine(handshakes);
+          }
+
+      }
    }
 }
--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -14,30 +14,34 @@ namespace HackerRank
    {
       static void Main(string[] args)
       {
-         Handshakes(Convert.ToInt32(Console.ReadLine()));
+         StrangeGrid();
 //         Console.ReadLine();
       }
 
-       public static void Handshakes(int numOfTestCases)
+       public static void StrangeGrid()
        {
-           for (int testNo = 0; testNo < numOfTestCases; testNo++)
-           {
-               var people = Convert.ToInt64(Console.ReadLine());
-               long handshakes = 0;
+           var inputLine = Console.ReadLine().Split(' ').Select(s => Convert.ToInt64(s)).ToArray();
+           var rowNo = inputLine[0];
+           var colNo = inputLine[1];
 
-               if (people >= 2)
-               {
-                   var half = Convert.ToInt64(Math.Ceiling((double) people/2));
-                   handshakes = ((half - 1)*people);
-                   if (people%2 == 0)
-                   {
-                       handshakes = handshakes + half;
-                   }
-               }
-               Console.WriteLine(handshakes);
+           long retValue = 0;
+
+           var tens = Convert.ToInt64(Math.Ceiling((double)rowNo/2)) - 1;
+           retValue = 10 * tens;
+           if (rowNo % 2 == 0)
+           {
+               retValue = retValue + (colNo*2) - 1;
+               // even row so odd numbers
            }
-       
+           else
+           {
+               // odd row so even numbers
+               retValue = retValue + (colNo * 2) - 2;
+           }
+           Console.WriteLine(retValue);
        }
+
+
 
        public static void MorganAndAString(int numOfTestCases)
       {
